4ccd24b8d5e6708ab131fd8564ad8ec495c51f59
Daan Luttik <dtluttik@gmail.com>
2021-10-05 17:56:12 +0200

    Fixed mypy issues

--- src/auto_optional/cst_helper.py
+++ src/auto_optional/cst_helper.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Sequence, Type
+from typing import List, Optional, Type
 
 import libcst as cst
 
@@ -7,15 +7,15 @@ def unwrap_commutative_binary_operator(
     operation_node: cst.BinaryOperation,
     operator: Optional[Type[cst.BaseBinaryOp]] = None,
 ) -> List[cst.BaseExpression]:
+    # Created a new variable to satisfy mypy
+    operator_not_null = operator or type(operation_node.operator)
+
     def unwrap_or_return(node: cst.BaseExpression) -> List[cst.BaseExpression]:
         if isinstance(node, cst.BinaryOperation) and isinstance(
-            node.operator, operator
+            node.operator, operator_not_null
         ):
-            return unwrap_commutative_binary_operator(node, operator)
+            return unwrap_commutative_binary_operator(node, operator_not_null)
         return [node]
-
-    if operator is None:
-        operator = type(operation_node.operator)
 
     return [
         *unwrap_or_return(operation_node.left),
